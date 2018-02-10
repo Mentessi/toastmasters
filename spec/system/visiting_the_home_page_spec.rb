d85e3f4a78909ee89e91822e84d11cf01a680ebd
Michael <mentessi@gmail.com>
2018-02-10 09:25:14 +0000

    Welcome page renamed to home page

--- spec/system/visiting_the_home_page_spec.rb
+++ spec/system/visiting_the_home_page_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'visiting welcome page' do 
+RSpec.describe 'visiting home page' do 
   scenario 'when viewing' do 
     visit('/') 
     expect(page).to have_content('Welcome')
   end
 end
+
