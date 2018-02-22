3d7d34b10a544ab7942ad58601370cffcb540ae3
Michael <mentessi@gmail.com>
2018-02-22 13:52:06 +0000

    create topic

--- spec/system/managing_topics_spec.rb
+++ spec/system/managing_topics_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe 'managing topics' do
     expect(page).to have_content('Topics')
     expect(page).to have_content('What is your favourite colour')
   end
+
+  scenario 'when creating a topic' do
+    visit topics_path
+    click_link 'New Topic'
+    click_button 'Create'
+    expect(page).to have_content "Name can't be blank"
+    fill_in 'Name', with: 'tell us a story'
+    click_button 'Create'
+    expect(current_path).to eq topics_path
+    expect(page).to have_content 'tell us a story'
+  end
 end
