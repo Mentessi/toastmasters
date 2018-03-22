5e921323d31884d96577ed5287eac077068a7eff
Michael <mentessi@gmail.com>
2018-03-22 21:25:48 +0000

    User email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  describe 'validations' do
+    context 'when email missing' do
+      subject { FactoryBot.build(:user, email: "") }
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when email is already taken' do
+      before do
+        FactoryBot.create(:user, email: 'hello@email.com' )
+      end
+      subject { FactoryBot.build(:user, email: 'hello@email.com' ) }
+      it { is_expected.to be_invalid }
+    end
+  end
 end
