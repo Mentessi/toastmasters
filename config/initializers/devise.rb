17ef4499284cf01ef9a8a7a65bfa4659ec6cd27f
mentessi <mentessi@gmail.com>
2018-04-24 08:20:03 +0100

    devise config - use env variable for email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'cfb1460fbed574c9672062bf7ee05cc1156dbb8f292a69ec9427ba90acaf9db06587f38d357335665ba60dba7a72a171d9ceca3338cac2030c74b5fd7b664d1e'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = [ENV 'MAILER_EMAIL_ADDRESS']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
