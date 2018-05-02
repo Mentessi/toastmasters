15a3a34ca2757e41407fbfc7329b4b4ea5c79b7e
mentessi <mentessi@gmail.com>
2018-05-02 11:24:42 +0100

    Topic requests specs

---
+++ spec/requests/topic_requests_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Topic requests', type: :request do
+  let!(:topic) { FactoryBot.create :topic }
+  let(:user) { FactoryBot.create :user }
+
+  describe 'get#show' do
+    let(:dispatch_request) { get topic_path(topic) }
+    it_behaves_like 'NOT only for logged in users'
+  end
+
+  describe 'get#new' do
+    let(:dispatch_request) { get new_topic_path }
+    it_behaves_like 'only logged in users'
+  end
+
+  describe 'post#create' do
+    let(:dispatch_request){post topics_path, params: { topic: {name: "here is a new topic"} } }
+    it_behaves_like 'only logged in users'
+  end
+
+  describe 'get#edit' do
+    let(:dispatch_request) { get edit_topic_path(topic) }
+
+    it_behaves_like 'only logged in users'
+  end
+
+  describe 'put#update' do
+    let(:dispatch_request) {
+      put topic_path(topic), params: { topic: {:name => "ammended topic"} }
+    }
+
+    it_behaves_like 'only logged in users'
+  end
+
+  describe 'delete#destroy' do
+    let(:dispatch_request) { delete topic_path(topic) }
+
+    it_behaves_like 'only logged in users'
+  end
+end
---
+++ spec/support/shared_examples/authentication_protected.rb
@@ -0,0 +1,19 @@
+RSpec.shared_examples 'only logged in users' do
+  it 'doesnt allow logged out users' do
+    dispatch_request
+    expect(response).to redirect_to(new_user_session_path)
+  end
+
+  it 'allows logged in users' do
+    login_as(user, scope: :user)
+    dispatch_request
+    expect(response).not_to redirect_to(new_user_session_path)
+  end
+end
+
+RSpec.shared_examples 'NOT only for logged in users' do
+  it 'allows logged out users' do
+    dispatch_request
+    expect(response).not_to redirect_to(new_user_session_path)
+  end
+end
