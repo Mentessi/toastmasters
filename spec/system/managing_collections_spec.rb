a33e8fa205d5f03a95a00c513ae86115b0ea8fe0
mentessi <mentessi@gmail.com>
2018-05-12 17:27:45 +0100

    update and destroy specs

--- spec/system/managing_collections_spec.rb
+++ spec/system/managing_collections_spec.rb
@@ -39,4 +39,37 @@ RSpec.describe 'managing collections' do
     click_button 'Create'
     expect(page).to have_content 'cat collection'
   end
+
+  scenario 'updating a collection' do
+    visit collection_path(collection)
+    expect(page).to_not have_link 'Edit Collection'
+
+    sign_in user
+    visit collection_path(collection)
+    click_link 'Edit Collection'
+    expect(current_path).to eq edit_collection_path(collection)
+    expect(page).to have_content 'dogs and cats'
+    fill_in 'Name', with: ''
+    click_button 'Update Collection'
+    expect(page).to have_content "Name can't be blank"
+    fill_in 'Name', with: 'rabbits'
+    click_button 'Update Collection'
+    expect(current_path).to eq collection_path(collection)
+    expect(page).to have_content 'rabbits'
+  end
+
+  scenario 'deleting a collection' do
+    # No delete button if not logged in
+    visit collection_path(collection)
+    expect(page).to have_no_button 'Delete Collection'
+
+    # can delete when logged in
+    sign_in user
+    visit collection_path(collection)
+    expect {
+      accept_confirm { click_on 'Delete Collection' }
+      expect(current_path).to eq collections_path
+    }.to change(Collection, :count).by(-1)
+    expect(page).to have_no_content 'dogs and cats'
+  end
 end
