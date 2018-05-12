f2987ea868a7c6a8e0a746de544530870c553cd5
mentessi <mentessi@gmail.com>
2018-05-12 16:14:55 +0100

    collection views

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Collection, type: :model do
+
+  describe 'valildations' do
+    context 'name' do
+      it "is required" do
+        collection = FactoryBot.build(:collection, name: '')
+        expect(collection).to be_invalid
+      end
+    end
+
+    context 'factory' do
+      subject { FactoryBot.build(:collection) }
+      it { is_expected.to be_valid }
+    end
+  end
+
+  describe 'associations' do
+    it { should have_and_belong_to_many(:topics) }
+  end
 end
