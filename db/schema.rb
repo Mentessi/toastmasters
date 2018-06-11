cb331335503ffe13f4b872ddf8065d3c2e6f3ac9
mentessi <mentessi@gmail.com>
2018-06-11 08:55:05 +0100

    db:migration collections belong to user

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180621065505) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_collections_on_user_id"
   end
 
   create_table "collections_topics", force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema.define(version: 20180621065505) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collections", "users"
   add_foreign_key "topics", "users"
 end
