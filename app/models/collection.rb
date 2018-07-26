155dea0b46d9758ce44f6cb01316b21193058c31
mentessi <mentessi@gmail.com>
2018-07-26 09:30:39 +0100

    Collection accepts nested attributes and allows destroy

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,5 @@ class Collection < ApplicationRecord
   has_many :topics, through: :collections_topics
   belongs_to :user
   validates :name, presence: true
+  accepts_nested_attributes_for :collections_topics, allow_destroy: true
 end
