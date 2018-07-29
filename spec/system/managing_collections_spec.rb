ccaa98910c6299180f279b787efe281da8021544
mentessi <mentessi@gmail.com>
2018-07-29 16:27:43 +0100

    Removed topic management references from collections spec

--- spec/system/managing_collections_spec.rb
+++ spec/system/managing_collections_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe 'managing collections' do
   let!(:collection1) { FactoryBot.create :collection, name: 'Dogs and cats' }
   let!(:collection2) { FactoryBot.create :collection, name: 'Rabbits and hamsters' }
-  let!(:collection3) { FactoryBot.create :collection, name: 'Monkeys and lamas', user: collection1.user }
   let!(:unrelated_topic){ FactoryBot.create :topic, name: 'unrelated' }
   let!(:topic){ FactoryBot.create :topic, name: 'dog topic' }
   let!(:user){ collection1.user }
-  let!(:user2){ collection2.user }
 
   scenario 'when viewing collections' do
     visit collections_path
@@ -83,36 +81,5 @@ RSpec.describe 'managing collections' do
       expect(current_path).to eq collections_path
     }.to change(Collection, :count).by(-1)
     expect(page).to have_no_content 'Dogs and cats'
-  end
-
-  scenario 'add a topic to a collection' do
-    # No add topic to collection option if not logged in
-    visit topic_path(topic)
-    expect(page).to have_no_content 'Add to collection'
-
-    # Can only add to own collections
-    sign_in user
-    visit topic_path(topic)
-    expect(page).to have_no_content('Rabbits and hamsters')
-
-    #Can add own topic to collection
-    sign_in user
-    visit topic_path(topic)
-    expect(page).to have_content('Add to collection')
-    expect(page).to have_content collection1.name
-    find('label', :text => collection1.name).click
-    click_button 'Update'
-    expect(current_path).to eq topic_path(topic)
-    expect(page).to have_content "collection(s) successfully updated"
-    expect(page).to have_field(collection1.name, checked: true, visible: false)
-
-    #can update multiple collections at once when logged in
-    find('label', :text => collection1.name).click
-    find('label', :text => collection3.name).click
-    click_button 'Update'
-    expect(current_path).to eq topic_path(topic)
-    expect(page).to have_content "collection(s) successfully updated"
-    expect(page).to have_field(collection1.name, checked: false, visible: false)
-    expect(page).to have_field(collection3.name, checked: true, visible: false)
   end
 end
