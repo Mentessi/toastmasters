73a92462760d14bc6958182b3625364c8f476ab7
mentessi <mentessi@gmail.com>
2018-08-03 07:51:51 +0100

    added repo directory

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.default_options = { from: 'from@example.com' }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
