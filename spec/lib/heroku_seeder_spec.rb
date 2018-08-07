f6147dedfb2fd1a962a21e8a535c60d235ad4aa2
mentessi <mentessi@gmail.com>
2018-08-07 15:06:59 +0100

    heroku seeder spec

---
+++ spec/lib/heroku_seeder_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'heroku_seeder'
+
+RSpec.describe 'heroku runner' do
+  context 'when running Heroku seeder' do
+    it 'runs without raising errors' do
+      expect { HerokuSeeder.run }.to_not raise_error
+    end
+  end
+end
+
