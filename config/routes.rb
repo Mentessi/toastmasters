1441f32320661b2b8f2db432ce4a0595d1f2451d
mentessi <mentessi@gmail.com>
2018-08-10 08:45:34 +0100

    routes: name changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :topics do
-    resources :collections_topics, only: [:create]
+    resources :collections_memberships, only: [:create]
   end
 
   resources :collections
