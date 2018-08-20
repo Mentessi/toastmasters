ef6f2eb4788e1a87c694381921354f2d4f1f09ee
mentessi <mentessi@gmail.com>
2018-08-20 09:43:41 +0100

    fix VariableNumber

--- lib/heroku_seeder.rb
+++ lib/heroku_seeder.rb
@@ -1,57 +1,57 @@
 class HerokuSeeder
   def self.run
 
-    user_1 = User.where(
+    user1 = User.where(
       email: 'michael@bluespot.io'
     ).first_or_create(
       username: 'Michael',
       password: 'password'
     )
 
-    user_2 = User.where(
+    user2 = User.where(
       email: 'neil@bluespot.io'
     ).first_or_create(
       username: 'Neil',
       password: 'password'
     )
 
-    collection_1 = Collection.where(
+    collection1 = Collection.where(
       name: 'Dog Collection'
     ).first_or_create(
-      user_id: user_1.id
+      user_id: user1.id
     )
 
     Collection.where(
       name: 'Cat Collection'
     ).first_or_create(
-      user_id: user_2.id
+      user_id: user2.id
     )
 
-    topic_1 = Topic.where(
+    topic1 = Topic.where(
       name: 'Dalmatian'
     ).first_or_create(
-      user_id: user_1.id
+      user_id: user1.id
     )
 
-    topic_2 = Topic.where(
+    topic2 = Topic.where(
       name: 'Poodle'
     ).first_or_create(
-      user_id: user_1.id
+      user_id: user1.id
     )
 
     Topic.where(
       name: 'Siamese'
     ).first_or_create(
-      user_id: user_2.id
+      user_id: user2.id
     )
 
     Topic.where(
       name: 'Bengal'
     ).first_or_create(
-      user_id: user_2.id
+      user_id: user2.id
     )
 
-    collection_1.topics << topic_1 unless collection_1.topics.include?(topic_1)
-    collection_1.topics << topic_2 unless collection_1.topics.include?(topic_2)
+    collection1.topics << topic1 unless collection1.topics.include?(topic1)
+    collection1.topics << topic2 unless collection1.topics.include?(topic2)
   end
 end
